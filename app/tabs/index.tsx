69957f50d2db39934668772bb011bdc5d11ea177
GuilhermeNantes <guinantes27@gmail.com>
2025-06-18 16:19:22 -0400

    fkdgldjgklkdfgd
    dfgdfgdfgdfgdfgd

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -9,25 +9,27 @@ import {
 } from "react-native";
 
 export default function home() {
+  const temas = ["acitio"]
   const[date,setDate] = useState()
   const[response,setResponse] = useState()
-
+  const[loading,setLoading] = useState(true)
   useEffect(() =>{
 
     const fetchDados = async () => {
-
-      const dateJson = await fetch("https://fakestoreapi.com/products")
-      
-      const convesion = await dateJson.json()
-
-      setDate(convesion)
-    }
-
+      try {
+        const res = await fetch("https://api.sampleapis.com/movies/animation");
+        const data = await res.json();
+        setDate(data);
+      } catch (error) {
+        console.error("Erro ao buscar filmes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
     fetchDados()
   
   },[]);
   console.log(date)
-
 
   return (
     <View></View>
